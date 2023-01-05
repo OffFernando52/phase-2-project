00a870d9f0129b5acc44c88aac317efacfb4ec17
Hunter Wood <hunter674530@gmail.com>
2023-01-05 12:46:58 -0600

    Derecks changes

--- phase-2-project/src/components/MovieCard.js
+++ phase-2-project/src/components/MovieCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Movies from "./Movies";
 
-function MovieCard({ movies }) {
+function MovieCard({ movies, nowClicked }) {
   console.log(movies);
   return (
     <div className="cards">
       {movies.map((movie) => (
         <Movies
+          nowClicked={nowClicked}
           movie={movie}
           key={movie.id}
           name={movie.original_title}
